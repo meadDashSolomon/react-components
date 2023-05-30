80ed044fa740dd447725c4fea410de7e6c385b2b
Mead Solomon <mead93@gmail.com>
2023-05-30 11:40:03 -0500

    assignment complete

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-// TODO
+const { useState }  = React;
+
+const groceryItems = ["fruit", "vegetables", "drinks"]
+
+const GroceryListItem = (props) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const style = {
+    fontWeight: isHovered ? 'bold' : 'none',
+  };
+
+  return (
+    <li style={style} onClick={() => setIsHovered(!isHovered)}>
+      {props.item}
+    </li>
+  );
+}
+
+const GroceryList = (props) => {
+  return (
+    <ul>
+      <h2>Groceries</h2>
+      {props.groceryItems.map((item) => (
+          <GroceryListItem item={item} />
+        ))}
+    </ul>
+  );
+}
+
+ReactDOM.render(
+  <GroceryList groceryItems={groceryItems} />,
+  document.getElementById("app")
+);
